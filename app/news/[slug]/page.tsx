2869b0538452f133736edb2f23cd2269b525cc1d
nnnnnnrum <2110370043g@kindai.ac.jp>
2024-10-04 22:41:46 +0900

    運営メンバー一覧のページ#21

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getNewsDetail } from "@/app/_libs/microcms";
+import { getNewsList } from "@/app/_libs/microcms";
 import Article from "@/app/components/Article";
 import ButtonLink from "@/app/components/ButtonLink";
 import styles from "./page.module.css";
@@ -9,6 +10,13 @@ type Props = {
         slug: string;
     };
 };
+
+export async function generateStaticParams() {
+    const newsList = await getNewsList();  
+    return newsList.contents.map((news: { id: string }) => ({
+        slug: news.id,
+    }));
+}
 
 export default async function Page({params}:Props){
     const data = await getNewsDetail(params.slug)
