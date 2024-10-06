50a9e613e95e816c71a4840943ffe17a5af72e50
y-murakuki <yuukimurakuki7@gmail.com>
2024-10-06 16:40:46 +0900

    TailWindcssで体裁を整えてみました

--- app/component/Header/index.tsx
+++ app/component/Header/index.tsx
@@ -1,12 +1,13 @@
 //import Image from "next/image";
 import Link from "next/link";
-import styles from "./index.module.css";
+//import styles from "./index.module.css";
+import "../../globals.css";
+import type {} from "next/app";
 
 export default function Header() {
   return (
-    //classNameは{styles.header}にする
-    <header className={styles.header}>
-      <Link href="/" className={styles.logoLink}>
+    <header className="subpixel-antialiased flex h-[70px] md:h-[60px] sm:h-[50px] px-6 py-4 z-1000 items-center justify-center bg-gradient-to-b from-[#c9a080] to-white">
+      <Link href="/" className="text-3xl font-bold text-white">
         NaniwaNOG
       </Link>
     </header>
