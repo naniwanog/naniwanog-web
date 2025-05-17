1afc171f367d5521cb7b0a743f825faf10de9232
taisei.kanie <taisei.kanie@bbix.net>
2025-05-17 13:46:51 +0900

    chore: add linter and prettier config

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:import/recommended',
+      'plugin:import/warnings',
+      'prettier',
+    ],
+  }),
+]
+
+export default eslintConfig
