902c968bcdd62f41ac6888d1196a52a913252b73
taisei.kanie <taisei.kanie@bbix.net>
2025-05-18 13:31:09 +0900

    fix: adjust container width for better responsiveness

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,14 +10,14 @@ type props = {
 export default function Container({ title, description, description2, children }: props) {
     return (
         <div className="bg-white px-6 py-16 lg:px-8">
-            <div className="mx-auto max-w-3xl text-base/7 text-gray-700">
+            <div className="mx-auto max-w-[80%] text-base/7 text-gray-700">
                 <h1 className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
                     {title}
                 </h1>
                 <p className="mt-6 text-xl/8">
                     {description}
                 </p>
-                <div className="mt-10 max-w-2xl">
+                <div className="mt-10 max-w-full">
                     <p>
                         {description2}
                     </p>
